10bad3d3fdd21de4cf9c98b8806aadad7a75f4a9
Julien <julienlfy@gmail.com>
2017-05-01 12:14:52 +0200

    Rewrite plugin using Object literal (btw plugin should be used as a 'singleton')

--- src/jq-anchor.js
+++ src/jq-anchor.js
@@ -1,115 +1,155 @@
+(function($) {
 
-function JQAnchor() {
-    this.observers = jQuery.Callbacks();
-}
+    if (!($.JQAnchor || false)) {
 
-JQAnchor.prototype.addObserver = function(callBack) {
-    this.observers.add(callBack);
-};
+        $.JQAnchor = {
 
-JQAnchor.prototype.run = function(){
-    this._detectHashchange();
-};
+            options: {
+            },
 
-JQAnchor.prototype.go = function(pageName, state) {
-    var hash = '';
-    if (pageName) {
-        hash = encodeURIComponent(pageName);
-    }
-    
-    var args = [];
-    for (var key in state) {
-        if (state.hasOwnProperty(key)) {
-            args.push(encodeURIComponent(key) + "=" + encodeURIComponent(state[key]));
-        }
-    }
-    
-    if (args) {
-        hash = hash + '?' + args.join("&");
-    }
-    
-    if (hash) {
-        window.location.hash = hash;
-    }
-};
+            construct: function () {
+                $.JQAnchor.observers = $.Callbacks();
+            },
+            
+            addObserver: function(callBack) {
+                $.JQAnchor.observers.add(callBack);
+            },
 
-JQAnchor.prototype._detectHashchange = function() {
-    
-    // Fire event on initial loading if hash is defined.
-    if (window.location.hash) {
-        var obj = this._parseHash(window.location.hash);
+            run: function(){
+                $.JQAnchor._detectHashchange();
+            },
+
+            goto: function(pageName, state) {
+                var JQAnchor = $.JQAnchor;
+                var hash = '';
+
+                if (pageName) {
+                    hash = encodeURIComponent(pageName);
+                }
+                
+                var args = [];
+                for (var key in state) {
+                    if (state.hasOwnProperty(key)) {
+                        args.push(encodeURIComponent(key) + "=" + encodeURIComponent(state[key]));
+                    }
+                }
+                
+                if (args) {
+                    hash = hash + '?' + args.join("&");
+                }
+                
+                JQAnchor.setHash(hash);
+            },
+
+            getHash: function() {
+                return window.location.hash || location.hash;
+            },
+
+            setHash: function(hash) {
+                var JQAnchor = $.JQAnchor;
+
+                if (typeof window.location.hash !== 'undefined') {
+                    if (window.location.hash !== hash) {
+                        window.location.hash = hash;
+                    }
+                } else if (location.hash !== hash) {
+                    location.hash = hash;
+                }
+
+                return hash;
+            },
+
+            _detectHashchange: function() {
+                var JQAnchor = $.JQAnchor;
+
+                var hash = JQAnchor.getHash();
+
+                // Fire event on initial loading if hash is defined.
+                if (hash) {
+                    var obj = JQAnchor._parseHash(hash);
+                    
+                    JQAnchor.observers.fire(obj.pageName, obj.args);
+                }
+                
+                // Fire event when hash change.
+                $(window).on('hashchange', function() {
+                    var h = JQAnchor.getHash();
+                    var obj = JQAnchor._parseHash(h);
+                    
+                    JQAnchor.observers.fire(obj.pageName, obj.args);
+                });
+            },
+
+            _parseHash: function(hash) {
+                var JQAnchor = $.JQAnchor;
+                var h = hash.trim();
+                
+                var sParams = "",
+                    pageName = "";
+                
+                if (h.length > 0) {
+                    
+                    // Remove leading '#'
+                    if (h.substr(0, 1) == "#") {
+                        h = h.substr(1);
+                    }
+                    
+                    // Is there a '?'
+                    var qm = h.indexOf("?");
+                    if (qm !== -1) {
+                        // No text before '?'
+                        if (qm == 0) {
+                            h = h.substr(1);
+                            sParams = h;
+                        } 
+                        // Text before '?'
+                        else {
+                            sParams = h.substr(qm + 1);
+                            pageName = h.substr(0, qm);
+                        }
+                    } 
+                    // No '?'
+                    else {
+                        pageName = h;
+                    }
+                }
+                
+                var args = JQAnchor._parseParams(sParams);
+                var obj = {
+                    'pageName': decodeURIComponent(pageName),
+                    'args': args
+                }
+                return obj;
+            },
+
+            _parseParams: function(sParams) {
+                var pieces = [], 
+                    data = {};
+                
+                if (sParams) {
+                    pieces = sParams.split("&")
+                }
+                
+                // process each query pair
+                for (var i = 0; i < pieces.length; i++) {
+                    var parts = pieces[i].split("=");
+                    if (parts.length < 2) {
+                        parts.push("");
+                    }
+                    data[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+                }
+                return data;
+            },
+
+
+        }; // End of plugin
+
         
-        this.observers.fire(obj.pageName, obj.args);
+        // Initialise
+        $.JQAnchor.construct();
+    }
+    else {
+        window.console && console.warn('$.JQAnchor has already been defined...');
     }
 
-    var _this = this;
-    
-    // Fire event when hash change.
-    jQuery(window).on('hashchange', function(){
-        var obj = _this._parseHash(location.hash);
-        
-        _this.observers.fire(obj.pageName, obj.args);
-    });
-};
-
-JQAnchor.prototype._parseHash = function(hash) {
-    var h = hash.trim();
-    
-    var sParams = "",
-        pageName = "";
-    
-    if (h.length > 0) {
-        
-        // Remove leading '#'
-        if (h.substr(0, 1) == "#") {
-            h = h.substr(1);
-        }
-        
-        // Is there a '?'
-        var qm = h.indexOf("?");
-        if (qm !== -1) {
-            // No text before '?'
-            if (qm == 0) {
-                h = h.substr(1);
-                sParams = h;
-            } 
-            // Text before '?'
-            else {
-                sParams = h.substr(qm + 1);
-                pageName = h.substr(0, qm);
-            }
-        } 
-        // No '?'
-        else {
-            pageName = h;
-        }
-    }
-    
-    var args = this._parseParams(sParams);
-    var obj = {
-        'pageName': decodeURIComponent(pageName),
-        'args': args
-    }
-    return obj;
-};
-
-
-JQAnchor.prototype._parseParams = function(sParams) {
-    var pieces = [], 
-        data = {};
-    
-    if (sParams) {
-        pieces = sParams.split("&")
-    }
-    
-    // process each query pair
-    for (var i = 0; i < pieces.length; i++) {
-        var parts = pieces[i].split("=");
-        if (parts.length < 2) {
-            parts.push("");
-        }
-        data[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
-    }
-    return data;
-};
-
+})(jQuery);
